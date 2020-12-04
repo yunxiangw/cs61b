346d280e1a95fc853cc8bb3ecdee3e65a4e89676
yunxianw <yxwang1995@gmail.com>
2020-12-03 17:48:15 -0800

    Fix Bug in printing info

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -70,12 +70,12 @@ public class NBody {
         }
         
         // Print info
-        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%d\n", allPlanets.length);
         StdOut.printf("%.2e\n", radius);
-        for (int i = 0; i < planets.length; i++) {
+        for (int i = 0; i < allPlanets.length; i++) {
             StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                          planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-                          planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+                          allPlanets[i].xxPos, allPlanets[i].yyPos, allPlanets[i].xxVel,
+                          allPlanets[i].yyVel, allPlanets[i].mass, allPlanets[i].imgFileName);   
         }
 	}
 }
