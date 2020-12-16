cb94dcef346aa30101e2dfdb2480aa8cb5690693
yunxianw <yxwang1995@gmail.com>
2020-12-15 19:50:28 -0800

    Complete proj1gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+
+    private String message;
+
+    /**
+     * @source: StudentArrayDequeLauncher.java
+     * */
+    @Test
+    public void testArrayDeque() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+
+        for (int i = 0; i < 20; i += 1) {
+            double randNum = StdRandom.uniform();
+
+            if (randNum < 0.25) {
+                sad.addLast(i);
+                ads.addLast(i);
+                message += "addLast(" + Integer.toString(i) + ")\n";
+            } else if (randNum >= 0.25 && randNum < 0.5){
+                sad.addFirst(i);
+                sad.addFirst(i);
+                message += "addLast(" + Integer.toString(i) + ")\n";
+            } else if (randNum >= 0.5 && randNum <= 0.75){
+                message += "removeFirst()\n";
+                assertEquals(message, sad.removeFirst(), ads.removeFirst());
+            } else {
+                message += "removeLast()\n";
+                assertEquals(message, sad.removeLast(), ads.removeLast());
+            }
+        }
+    }
+}
