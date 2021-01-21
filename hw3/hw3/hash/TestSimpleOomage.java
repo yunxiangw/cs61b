e02854f525c336220ff11fe5f2a7b44abe2bca28
yunxiang <yxwang1995@gmail.com>
2021-01-21 01:02:14 -0800

    Complete hw3

--- hw3/hw3/hash/TestSimpleOomage.java
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -3,7 +3,6 @@ package hw3.hash;
 import org.junit.Test;
 
 
-import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
@@ -25,9 +24,9 @@ public class TestSimpleOomage {
     @Test
     public void testHashCodePerfect() {
         HashSet<Integer> hashcode = new HashSet<>();
-        for (int r = 0; r <= 255; r+=5) {
-            for (int g = 0; g <= 255; g+=5) {
-                for (int b = 0; b <= 255; b+=5) {
+        for (int r = 0; r <= 255; r += 5) {
+            for (int g = 0; g <= 255; g += 5) {
+                for (int b = 0; b <= 255; b += 5) {
                     SimpleOomage oo = new SimpleOomage(r, g, b);
                     assertFalse(hashcode.contains(oo.hashCode()));
                     hashcode.add(oo.hashCode());
@@ -57,8 +56,7 @@ public class TestSimpleOomage {
         assertTrue(hashSet.contains(ooA2));
     }
 
-    /* TODO: Uncomment this test after you finish haveNiceHashCode Spread in OomageTestUtility */
-    /*@Test
+    @Test
     public void testRandomOomagesHashCodeSpread() {
         List<Oomage> oomages = new ArrayList<>();
         int N = 10000;
@@ -68,7 +66,7 @@ public class TestSimpleOomage {
         }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
-    }*/
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
