310596166fd69aba9108fec2502f1b7c41ce3261
yunxiang <yxwang1995@gmail.com>
2021-01-21 13:45:10 -0800

    Complete hw2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -18,9 +18,8 @@ public class PercolationStats {
             Percolation sys = pf.make(N);
             while (!sys.percolates()) {
                 sys.open(StdRandom.uniform(N), StdRandom.uniform(N));
-                threshold[i]++;
             }
-            threshold[i] /= (N * N);
+            threshold[i] = (double) sys.numberOfOpenSites() / (N * N);
         }
 
     }
