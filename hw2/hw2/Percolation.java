83d08e18e00c6c8b091b27c01be74b0f1da7881d
yunxiang <yxwang1995@gmail.com>
2021-01-20 19:58:59 -0800

    Complete hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,79 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private WeightedQuickUnionUF checkFull;
+    private WeightedQuickUnionUF checkPercolate;
+    private boolean[][] records;
+    private int N;
+    private int count;
+
+    /* Create N-by-N grid (plus two virtual sites), with all sites initially blocked */
+    public Percolation(int N) {
+        checkFull = new WeightedQuickUnionUF(N * N + 1);
+        checkPercolate = new WeightedQuickUnionUF(N * N + 2);
+        records = new boolean[N][N];
+        count = 0;
+        this.N = N;
+        // checkPercolate has two virtual sites that are connected to top and bottom row respectively
+        // checkFull has one virtual site that is connected to top row
+        for (int i = 1; i < N + 1; i++) {
+            checkPercolate.union(0, i);
+            checkFull.union(0, i);
+        }
+        for (int i = N * (N - 1) + 1; i < N * N + 1; i++) {
+            checkPercolate.union(N * N + 1, i);
+        }
+    }
+
+    /* Open the site (row, col) if it is not open already */
+    public void open(int row, int col) {
+        if (isOpen(row, col)) return;
+        records[row][col] = true;
+        if (row - 1 >= 0 && isOpen(row - 1, col)) {
+            checkPercolate.union(xyToIndex(row, col), xyToIndex(row - 1, col));
+            checkFull.union(xyToIndex(row, col), xyToIndex(row - 1, col));
+        }
+        if (row + 1 <= N - 1 && isOpen(row + 1, col)) {
+            checkPercolate.union(xyToIndex(row, col), xyToIndex(row + 1, col));
+            checkFull.union(xyToIndex(row, col), xyToIndex(row + 1, col));
+        }
+        if (col - 1 >= 0 && isOpen(row, col - 1)) {
+            checkPercolate.union(xyToIndex(row, col), xyToIndex(row, col - 1));
+            checkFull.union(xyToIndex(row, col), xyToIndex(row, col - 1));
+        }
+        if (col + 1 <= N - 1 && isOpen(row, col + 1)) {
+            checkPercolate.union(xyToIndex(row, col), xyToIndex(row, col + 1));
+            checkFull.union(xyToIndex(row, col), xyToIndex(row, col + 1));
+        }
+        count++;
+    }
+
+    /* Check the site (row, col) is open or not */
+    public boolean isOpen(int row, int col) {
+        return records[row][col];
+    }
+
+    /* Check the site (row, col) is full or not */
+    public boolean isFull(int row, int col) {
+        return isOpen(row, col) && checkFull.connected(0, xyToIndex(row, col));
+    }
+
+    /* Number of open sites */
+    public int numberOfOpenSites() {
+        return count;
+    }
+
+    /* Check the system is percolate or not */
+    public boolean percolates() {
+        return checkPercolate.connected(0, checkPercolate.count() - 1);
+    }
+
+    /* Calculate index given the row and col */
+    private int xyToIndex(int row, int col) {
+        return row * N + col + 1;
+    }
+
+    public static void main(String[] args) {
+        Percolation system = new Percolation(5);
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,53 @@
 package hw2;
+import java.lang.IllegalArgumentException;
+import java.lang.Math;
+import edu.princeton.cs.introcs.StdRandom;
 
 public class PercolationStats {
 
+    private int[] threshold;
+    private int T;
+
+    /* perform T independent experiments on an N-by-N grid */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N and T must be larger than 0");
+        }
+        threshold = new int[T];
+        this.T = T;
+        for (int i = 0; i < T; i++) {
+            Percolation sys = pf.make(N);
+            while(!sys.percolates()) {
+                sys.open(StdRandom.uniform(N), StdRandom.uniform(N));
+                threshold[i]++;
+            }
+        }
+
+    }
+
+    public double mean() {
+        double sum = 0;
+        for (int x: threshold) {
+            sum += x;
+        }
+        return sum / T;
+    }
+
+    public double stddev() {
+        double sigma = 0;
+        double mu = mean();
+        for (int x: threshold) {
+            sigma += (x - mu) * (x - mu);
+        }
+        sigma = Math.sqrt(sigma / (T - 1));
+        return sigma;
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 }
